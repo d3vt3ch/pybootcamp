c4504291d489ca3c4b417e33630dc55bb978607e
d3vt3ch <200840704+d3vt3ch@users.noreply.github.com>
2026-04-02 13:47:57 +0800

    complete day2 work

--- py/conditional_statements.py
+++ py/conditional_statements.py
@@ -1,10 +1,53 @@
-weather ="sunny"
-temperatture = 75
+# weather ="sunny"
+# temperatture = 75
 
-if weather == "sunny":
-    if temperatture > 70:
-        print("It's sunny and warm")
-    else:
-        print("It's sunny but cool")
+# if weather == "sunny":
+#     if temperatture > 70:
+#         print("It's sunny and warm")
+#     else:
+#         print("It's sunny but cool")
 
-        
+# else:
+#     print("Its not sunny")
+
+# weight = float(input("Enter your weight (kg): "))
+# height = float(input("Enter your height (cm): "))           
+
+while True:
+    try:
+        weight = float(input("Enter your weight (kg): "))
+        if weight >0:
+            break
+        else:
+            print("Weight must be more than 0")
+    except ValueError:
+        print("Please enter valid number!")
+
+while True:
+    try:
+        height = float(input("Enter your height (cm): "))
+        if height >0:
+            break
+        else:
+            print("Height must be more than 0")
+    except ValueError:
+        print("Please enter valid number!")
+
+
+
+# Calculate BMI formula
+bmi = weight / ((height/100) ** 2)
+
+# Output result
+print(f"Your BMI is: {bmi:.2f}")
+
+if bmi>30.0:
+    print("Your BMI is Overweight")
+elif bmi<18.5:
+    print("Your BMI is Underweight")
+elif bmi<=24.9:
+    print("Your BMI is Normal")
+elif bmi<=29.9:
+    print("Your BMI is Overweight")
+else:
+    print("Try again")
