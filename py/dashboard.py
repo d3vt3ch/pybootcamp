a8aff2fa3c3b2579b933a8a959c63f8c59c1de10
d3vt3ch <200840704+d3vt3ch@users.noreply.github.com>
2026-04-13 04:57:06 +0000

    copy back the day 9 slide code, need to rerun day 5 again to make it run

--- py/dashboard.py
+++ py/dashboard.py
@@ -7,7 +7,7 @@ st.title(" Personal Dashboard")
 st.sidebar.header("Personal Information")
 
 name = st.sidebar.text_input("Your Name")
-age = st.sidebar.number_input("Your Age",min_value=1 max_value=120, value=25)
+age = st.sidebar.number_input("Your Age",min_value=1, max_value=120, value=25)
 
 favorite_color = st.sidebar.color_picker("Favorite Color", "#FF6B6B")
 
@@ -30,7 +30,7 @@ if name:
     with col2:
         st.metric("Hobbies", len(hobbies))
 
-    with cold3:
+    with col3:
         birth_year = datetime.datetime.now().year - age
         st.metric("Birth Year", birth_year)
 
@@ -41,7 +41,7 @@ if name:
     # Display hobbies
     if hobbies:
         st.subheader("Your Hobbies")
-        for hobbiy in hobbies:
+        for hobby in hobbies:
             st.write(f"* {hobby}")
 
     # Fun Fact
@@ -52,4 +52,3 @@ if name:
 else:
     st.info("Please enter your name in the sidebar to get started!")
 
-    
